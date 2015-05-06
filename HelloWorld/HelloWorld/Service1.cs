c785b2a07d800e1cd6a870342fe631f3232825d5
sosam29 <sosam29@gmail.com>
2015-05-05 22:27:09 -0400

    Revert "Revert "Demo_MT""
    This reverts commit 563645d6f858fff6b1c6b31decf866fd1bb9eb72.

---
+++ HelloWorld/HelloWorld/Service1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+
+namespace HelloWorld
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            Stopwatch tmr = new Stopwatch();
+            tmr.Start();
+            Random rdm = new Random();
+            int rdmnbr = rdm.Next(1, 5);
+            int rdmnbr2 = rdm.Next(2000, 3000);
+            Thread.Sleep(rdmnbr *100);
+            return string.Format("You entered: {0}- time taken is {1}", rdmnbr2, tmr.ElapsedMilliseconds);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+        public string getEmployDetails()
+        {
+            sampleclass sc = new sampleclass {
+               firstname = "sam",
+               lastname ="sonawane",
+               middleinitial ="p",
+               old= 42,
+            };
+
+            return sc.ToString();
+            //return "First name is " + sc.firstname + " last name is" + sc.lastname + " middle initial " + sc.middleinitial + " with age " + sc.old;
+        }
+    }
+}
---
+++ HelloWorld/TheadingDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TheadingDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("TheadingDemo")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5b14d294-aa29-4e69-9e6b-97a83099a517")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
